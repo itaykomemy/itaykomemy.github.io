52b4138d62d3205a8913817ba461d3a860cb7d97
itayk <itayk@ironscales.com>
2016-09-22 14:42:39 +0300

    remove scoped service worker

--- scripts/app.js
+++ scripts/app.js
@@ -6,12 +6,4 @@ if ('serviceWorker' in navigator) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
     });
-
-    navigator.serviceWorker.register('/scripts/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-    });
 }
